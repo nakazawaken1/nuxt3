4b75295cc0cdb96776621eadf46c5c0a9ce4ce45
nakazawaken1 <nakazawaken1@gmail.com>
2022-01-05 01:36:33 +0000

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  vite: {
+    server: {
+      hmr: {
+        protocol: 'ws'
+      }
+    }
+  }
+})
